0d600dbab6aa9e34fdc7a71354f54beb20a561e7
Fred Hornsey <fred@hornsey.us>
2021-10-16 17:54:25 -0500

    Make All BridleErrors Optionally Have Location

--- bridle/errors.py
+++ bridle/errors.py
@@ -2,8 +2,17 @@ from .utils import is_sequence
 
 
 class BridleError(Exception):
-    def __init__(self, fmt, *args):
-        self.message = fmt.format(*args)
+    def __init__(self, fmt, *args, location=None):
+        self.message_without_location = fmt.format(*args)
+        self.set_location(location)
+
+    def set_location(self, location):
+        # TODO: Make use of this for Errors not based on LocationError
+        self.location = location
+        if location is not None:
+            self.message = '{}: {}'.format(location, self.message_without_location)
+        else:
+            self.message = self.message_without_location
 
     def __str__(self):
         return self.message
@@ -20,9 +29,7 @@ class ErrorsReported(BridleError):
 
 class LocationError(BridleError):
     def __init__(self, location, fmt, *args):
-        self.location = location
-        self.message_without_location = fmt.format(*args)
-        super().__init__('{}: ' + fmt, location, *args)
+        super().__init__(fmt, *args, location=location)
 
 
 class ParseError(LocationError):
@@ -47,7 +54,7 @@ class SemanticError(LocationError):
 
 class RedefintionError(SemanticError):
     """\
-    Happens when a node has the same name as an existing name when it
+    Happens when a node has the same name as an existing node when it
     shouldn't. This is allowed when the two nodes are modules (assuming the
     module contents don't conflict) and when one or both nodes are forward
     declarations.
