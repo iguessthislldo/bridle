9720d9b2a9aa8e11811a720e260868f0a3af3f7f
Fred Hornsey <fred@hornsey.us>
2022-01-22 14:25:38 -0600

    Add `dry-run` Subcommand

--- bridle/__main__.py
+++ bridle/__main__.py
@@ -3,11 +3,16 @@ from argparse import ArgumentParser
 from .dump_tree import add_dump_tree_subcmd
 from .cdr.data_dumper import add_dump_data_subcmd
 from .lsp import add_lang_server_subcmd
+from .type_files import add_type_file_argument_parsing, type_files_to_trees
 
 
 def main():
     argparser = ArgumentParser(description='OMG IDL Analysis Tool')
     subcmds = argparser.add_subparsers(dest='subcommand', required=True)
+
+    subcmd = subcmds.add_parser('dry-run', help='Just parse type files')
+    subcmd.set_defaults(subcmd=type_files_to_trees)
+    add_type_file_argument_parsing(subcmd)
 
     add_dump_tree_subcmd(subcmds)
     add_dump_data_subcmd(subcmds)
